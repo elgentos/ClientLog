1470fbb1e4cb90e5e18683afc45b59ebbac4fd57
Elgentos Development <dev@elgentos.nl>
2012-11-19 09:09:34 +0100

    Added a size limitation to prevent people from fulling up the server HD with random bloated requests

--- app/code/community/Elgentos/ClientLog/controllers/IndexController.php
+++ app/code/community/Elgentos/ClientLog/controllers/IndexController.php
@@ -2,10 +2,14 @@
 
 class Elgentos_ClientLog_IndexController extends Mage_Core_Controller_Front_Action
 {
-	public function indexAction() {
-		print 'This page is not supposed to be reached this way. Now shoo!';
-		if(Mage::getStoreConfig('dev/log/clientside')) {
-			Mage::log($_POST,null,'clientside_javascript_error.log');
-		}
-	}
+        public function indexAction() {
+                print 'This page is not supposed to be reached this way. Now shoo!';
+                if(Mage::getStoreConfig('dev/log/clientside')) {
+                        if($_SERVER['CONTENT_LENGTH']<51200) {
+                                Mage::log($_POST,null,'clientside_javascript_error.log');
+                        } else {
+                                Mage::log('Post body was larger than 50kb; ignored.',null,clientside_javascript_error.log');
+                        }
+                }
+        }
 }
